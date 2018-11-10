d4ba5323c2bacb3059f8bbfc284af742189e5535
Mateus Gusmão <teumarques14@gmail.com>
2018-11-09 22:50:38 -0300

    Feito

--- src/main/java/br/edu/ifpe/jpa/jinq/ReportJinq.java
+++ src/main/java/br/edu/ifpe/jpa/jinq/ReportJinq.java
@@ -4,6 +4,12 @@ import java.util.Date;
 import java.util.List;
 
 import br.edu.ifpe.jpa.IReportGenerator;
+import br.edu.ifpe.jpa.entities.Account;
+import br.edu.ifpe.jpa.entities.Client;
+import java.util.stream.Collectors;
+import java.util.List;
+
+import br.edu.ifpe.jpa.IReportGenerator;
 import br.edu.ifpe.jpa.entities.Client;
 
 public class ReportJinq implements IReportGenerator {
@@ -14,21 +20,45 @@ public class ReportJinq implements IReportGenerator {
 	public List<String> getClientNames(Date initialDate, Date finalDate) {
 		return helper.execute(Client.class, streams ->
 			streams
-				.where(client -> client.getBirthDate().after(initialDate))
-				.where(client -> client.getBirthDate().before(finalDate))
-				.select(Client::getName)
-				.sortedBy(client -> client)
-				.toList()
+		             .where(client -> client.getBirthDate().after(initialDate))
+		        .where(client -> client.getBirthDate().before(finalDate))
+			.select(Client::getName)
+			.sortedBy(client -> client)
+			.toList()
 		); 
 	}
 	
+	// Retorna valor total em dinheiro que o cliente possui no banco, somandotodas as suas contas
+	
 	@Override
-	public double getClientTotalCash(String email) {
-		throw new UnsupportedOperationException("Not implemented yet.");
-	}
+         public double getClientTotalCash(String email) {
 
+        return helper.execute(Account.class,
+                streams
+                -> streams
+                   .where(account -> account.getClient().getEmail().equals(email))
+                   .sumDouble(Account::getBalance)
+        );
+
+    }
+
+	
+	//Retorna uma lista (ranking) com os e-mails dos clientes que possuem mais dinheiro no banco, somando todas suas contas.
 	@Override
-	public List<String> getBestClientsEmails(int agency, int rankingSize) {
-		throw new UnsupportedOperationException("Not implemented yet.");
-	}
+    public List<String> getBestClientsEmails(String agency, int rankingSize) {
+
+        List<String> emails = null;
+        emails.addAll(
+                helper.execute(Account.class,
+                        streams
+                        -> streams
+                           .sortedBy(Account::getBalance)
+                           .limit(rankingSize)
+                           .where(a -> a.getAgency().equals(agency))
+                           .select(a -> a.getClient().getEmail())
+                           .sumDouble(Account::getBalance)
+                     ));
+        return emails;
+
+    }
 }
--- src/main/java/br/edu/ifpe/jpa/querydsl/ReportQuerydsl.java
+++ src/main/java/br/edu/ifpe/jpa/querydsl/ReportQuerydsl.java
@@ -2,6 +2,7 @@ package br.edu.ifpe.jpa.querydsl;
 
 import static com.querydsl.core.group.GroupBy.*;
 
+
 import java.util.Date;
 import java.util.List;
 
@@ -10,10 +11,12 @@ import com.querydsl.jpa.impl.JPAQuery;
 
 import br.edu.ifpe.jpa.IReportGenerator;
 import br.edu.ifpe.jpa.entities.QClient;
+import br.edu.ifpe.jpa.entities.QAccount;
 
 public class ReportQuerydsl implements IReportGenerator {
 
 	static EntityManagerHelper helper = EntityManagerHelper.getInstance();
+	
 	
 	@Override
 	public List<String> getClientNames(Date initialDate, Date finalDate) {
@@ -29,13 +32,38 @@ public class ReportQuerydsl implements IReportGenerator {
 			);
 	}
 	
+	
+	//Retorna valor total em dinheiro que o cliente possui no banco, somando todas as suas contas
+	
 	@Override
 	public double getClientTotalCash(String email) {
-		throw new UnsupportedOperationException("Not implemented yet.");
+            QAccount account = QAccount.account;
+          return  helper.execute(query
+            -> query
+                  .select(account.balance.sum())
+                  .from(account)
+                  .where(account.client.email.eq(email))
+                  .groupBy(account.balance)
+                  .fetchOne()
+            );
 	}
+	
+//Retorna uma lista (ranking) com os e-mails dos clientes que possuem mais dinheiro no banco, somando todas suas contas
 
 	@Override
-	public List<String> getBestClientsEmails(int agency, int rankingSize) {
-		throw new UnsupportedOperationException("Not implemented yet.");
+	public List<String> getBestClientsEmails(String agency, int rankingSize){
+            QAccount account = QAccount.account;
+            
+         return  helper.execute(query
+            -> query
+                         .select(account.client.email)
+                         .from(account)
+                         .where(account.agency.eq(agency))
+                         .groupBy(account.balance.sum())
+                         .orderBy(account.balance.asc())
+                         .limit(rankingSize)
+                         .fetch()
+            ); 
+            
 	}
 }
